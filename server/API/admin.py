e50affe902ee50b23f2b4c5919002e32ad4a7e8b
Jangwon <wkddnjset@gmail.com>
2018-04-10 02:36:23 +0900

    2018-04-10

--- server/API/admin.py
+++ server/API/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email','phone', 'img')
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'actor', 'img')
+
+class CarrerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'actor', 'year', 'content')
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'actor', 'video')
+
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Carrer, CarrerAdmin)
+admin.site.register(Video, VideoAdmin)
--- server/API/models.py
+++ server/API/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Actor(models.Model):
+    name = models.CharField(max_length=45)
+    email = models.CharField(max_length=45)
+    phone = models.CharField(max_length=45)
+    img = models.ImageField()
+
+    def __str__(self):
+        return str(self.name)
+
+class Image(models.Model):
+    actor = models.ForeignKey(Actor)
+    img = models.ImageField()
+
+    def __str__(self):
+        return str(self.actor)
+
+
+class Carrer(models.Model):
+    actor = models.ForeignKey(Actor)
+    year = models.CharField(max_length=4)
+    content = models.TextField()
+
+    def __str__(self):
+        return str(self.actor)
+
+
+class Video(models.Model):
+    actor = models.ForeignKey(Actor)
+    video = models.CharField(max_length=45)
+
+    def __str__(self):
+        return str(self.actor)
