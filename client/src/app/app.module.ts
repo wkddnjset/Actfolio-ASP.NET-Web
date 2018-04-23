db5b2591ca56d494a520be0727da0be16c4f3a55
Jangwon <wkddnjset@gmail.com>
2018-04-23 11:30:42 +0900

    Update

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './detail/detail.component';
+import { HttpClientModule } from '@angular/common/http';
 
 import { NgxCarouselModule } from 'ngx-carousel';
 import 'hammerjs';
@@ -19,6 +20,7 @@ import 'hammerjs';
     AppRoutingModule,
     BrowserModule,
     NgxCarouselModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
